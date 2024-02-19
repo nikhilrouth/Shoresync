742cc3c8efafe777bd8d329adb915ba4f76f543f
prithvi002 <prithvik@vt.edu>
2024-02-19 16:56:40 -0500

    Added previous and reset buttons with functionality, wrote a condition such that form proceeds to next step if atleast one checkbox is selected in landuseform, created a global state in App.jsx such that it can have complete form data in the submit page

--- src/components/FinalSubmitForm.jsx
+++ src/components/FinalSubmitForm.jsx
@@ -2,37 +2,18 @@ import React, { useEffect, useState } from 'react';
 import CheckboxItem from './CheckBoxItem';
 import './FinalSubmitForm.css'; // Import the CSS file
 
-const FinalSubmitForm = () => {
-//   const [checkedItems, setCheckedItems] = useState({
-//     forest: false,
-//     scrubShrub: false,
-//     grass: false,
-//     agriculture: false,
-//     residential: false,
-//     commercial: false,
-//     industrial: false,
-//     marshIsland: false,
-//     bareLot: false,
-//     timberedClearCuts: false,
-//     pavedAreas: false,
-//     unknownLandUse: false,
-//   });
+const FinalSubmitForm = (props) => {
+const handleSubmitClick = () => {
+  alert('Form Submitted');
+};
 
-//   const handleCheckboxChange = (name) => {
-//     setCheckedItems((prevCheckedItems) => ({
-//       ...prevCheckedItems,
-//       [name]: !prevCheckedItems[name],
-//     }));
-//   };
+const handleReset = () => {
+  alert('Reset Clicked');
+};
 
-//   const handleContinueClick = () => {
-//     console.log('Checked Items:', checkedItems);
-//     // Add your logic here for what happens when the user clicks Continue
-//   };
-
-/// test adding
-
-
+const handlePrevious = () => {
+  props.setFormComponent(2);
+}
 
 useEffect(() => {
 
@@ -53,9 +34,23 @@ useEffect(() => {
             </button>
         </div>
         <div>
-            <button type="button" className="form-button2">
+
+            {/* <button type="button" className="form-button2">
+            Submit Final Data
+            </button> */}
+            <button type="reset" onClick={handlePrevious} className="form-button">Previous</button>
+            &nbsp;
+            &nbsp;
+            <button type="reset" onClick={handleReset} className="form-button">Reset</button>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <button type="button"  onClick={handleSubmitClick} className="form-button2">
             Submit Final Data
             </button>
+            
+            
         </div>
         
 
--- src/components/LandUseForm.jsx
+++ src/components/LandUseForm.jsx
@@ -1,23 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import CheckboxItem from './CheckBoxItem';
 import './LandUseForm.css'; // Import the CSS file
+import landUseData from '../app-static-data/appdata';
 
 const LandUseForm = (props) => {
 
-  const [checkedItems, setCheckedItems] = useState({
-    forest: false,
-    scrubShrub: false,
-    grass: false,
-    agriculture: false,
-    residential: false,
-    commercial: false,
-    industrial: false,
-    marshIsland: false,
-    bareLot: false,
-    timberedClearCuts: false,
-    pavedAreas: false,
-    unknownLandUse: false,
-  });
+  const [checkedItems, setCheckedItems] = useState(landUseData);
 
 
     const handleCheckboxChange = (name) => {
@@ -31,55 +19,88 @@ const LandUseForm = (props) => {
         sessionStorage.setItem('landUse', JSON.stringify(updatedCheckedItems));
         // console.log("Updated checked items:", updatedCheckedItems);
         return updatedCheckedItems;
-      });
-
-      
+      });   
     };
 
 
 
-  const fetchUserData = () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(checkedItems),
-    };
-    fetch("http://localhost:5000/api/addFormData",  requestOptions)
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-          console.log("data",data)
-        })
-  }
+  // const fetchUserData = () => {
+  //   const requestOptions = {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify(checkedItems),
+  //   };
+  //   fetch("http://localhost:5000/api/addFormData",  requestOptions)
+  //       .then(response => {
+  //         return response.json()
+  //       })
+  //       .then(data => {
+  //         console.log("data",data)
+  //       })
+  // }
+
 
   const handleContinueClick = () => {
-    console.log('Checked Items:', checkedItems);
-    sessionStorage.setItem('landUse', JSON.stringify(checkedItems));
+    console.log('Checked Items:', checkedItems);  
+    // Check if at least one item is checked
+    const isAtLeastOneChecked = Object.values(checkedItems).some(value => value === true);
+    if (isAtLeastOneChecked) {
 
-    
-    props.setFormComponent(1);
-    
-    // Add your logic here for what happens when the user clicks Continue
-    //navigate("/BankAttributes")
+      //session storage upodated
+      sessionStorage.setItem('landUse', JSON.stringify(checkedItems));
 
+      // Use a callback function in setAllFormsData to log the updated value
+      props.setAllFormsData(prevData => {
+        const updatedData = { ...prevData, landUse: checkedItems };
+        console.log("All Forms Data:", updatedData);
+        return updatedData;
+      });
+
+      //setting form to next form 
+      props.setFormComponent(1);
+    } else {
+      alert("Select atleast One");
+    }
+  };
+  
+
+
+  const handleReset = () => {
+    setCheckedItems({
+      forest: false,
+      scrubShrub: false,
+      grass: false,
+      agriculture: false,
+      residential: false,
+      commercial: false,
+      industrial: false,
+      marshIsland: false,
+      bareLot: false,
+      timberedClearCuts: false,
+      pavedAreas: false,
+      unknownLandUse: false,
+    });
+    sessionStorage.removeItem('landUse');
+    // Create a copy of the data object
+    // const newData = { ...props.allFormsData };
+    // // Delete the key from the copy
+    // delete newData[keyToDelete];
+    // // Update the state with the modified data
+    // setData(newData);
   };
 
   useEffect(() => {
     if(sessionStorage.getItem('landUse') !== null ){
     var recieved = sessionStorage.getItem('landUse');
     var parsedRecieved = JSON.parse(recieved);
-    setCheckedItems(parsedRecieved);
-    
-    }
-
-
-    
-   
+    setCheckedItems(parsedRecieved); 
+    }  
   },[]);   
 
+
+
   return (
-    <div className="form-container">
+    <div className="form-container" style={{paddingBottom: '60px'}}>
       <h2 className="form-header">Check the corresponding riparian land use classes</h2>
       <form>
         {Object.entries(checkedItems).map(([key, value]) => (
@@ -90,9 +111,19 @@ const LandUseForm = (props) => {
             onChange={handleCheckboxChange}
           />
         ))}
-        <button type="button" onClick={handleContinueClick} className="form-button">
+        {/* <button type="button" onClick={handleContinueClick} className="form-button">
           Continue
-        </button>
+        </button> */}
+
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+
+          <button type="button"  onClick={handleContinueClick} className="form-button">
+            Continue
+          </button>
+          &nbsp;
+          &nbsp;
+          <button type="reset" onClick={handleReset} className="form-button">Reset</button>
+      </div>
       </form>
     </div>
   );
